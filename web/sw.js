2b74dfa7d963c832335aa5438f52cc4bf5640179
sahusunny623-pixel <sahusunny623@gmail.com>
2026-09-12 18:27:54 +0530

    Add PWA manifest and service worker

---
+++ web/sw.js
@@ -0,0 +1,3 @@
+const CACHE='hisabsaathi-v1';
+self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(['/','/manifest.webmanifest']))));
+self.addEventListener('fetch',e=>{ if(e.request.method!=='GET') return; e.respondWith(fetch(e.request).catch(()=>caches.match(e.request))); });
